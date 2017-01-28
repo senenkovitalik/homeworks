65c9207dda9626e83b1c99801c99e1c124963c93
Vitaliy Senenko <senenkovitalik@gmail.com>
2017-01-28 19:39:25 +0200

    PHP 14.01 Fix task 6

--- 14.01/functions_forms_tasks/6/serverside.php
+++ 14.01/functions_forms_tasks/6/serverside.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>6</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
 <?php
 $target_dir = "gallery/";
 $files = $_FILES["photos"];
@@ -28,37 +36,47 @@ function loadImages($files, $directory) {
 		$uploadOk = 1;
 
 		if (isset($_POST["submit"])) {
+
+			// if something wrong - skip this file
+			if ($file["error"] == 1) { continue; }
+
 			// Check if image file is a actual image or fake image
 			$check = getimagesize($file["tmp_name"]);
 			if ($check !== false) {
-		        echo "File is an image - " . $check["mime"] . ".";
+		        // echo "File is an image - " . $check["mime"] . ".";
 		        $uploadOk = 1;
 		    } else {
-		        echo "File is not an image.";
+		        echo "File " . $file["name"] . " is not an image.<br>";
 		        $uploadOk = 0;
 		    }
 
 		    // Check if file already exists
 			if (file_exists($target_file)) {
-				echo "Sorry, file already exists.";
+				echo "Sorry, file " . $file["name"] . " already exists.<br>";
 				$uploadOk = 0;
 			}
 
+			// Check file size
+			if ($file["size"] > 1.5*1000000) {
+			    echo "Sorry, your file " . $file["name"] . " is too large.<br>";
+			    $uploadOk = 0;
+			}
+
 			// Allow certain file formats
-			$imageFileType = pathinfo("$target_file, PATHINFO_EXTENSION");
+			$imageFileType = pathinfo("$target_file", PATHINFO_EXTENSION);
 			if ($imageFileType != "jpg" && 
 				$imageFileType != "png" && 
 				$imageFileType != "jpeg" && 
 				$imageFileType != "gif") 
 			{
-				echo "Sorry, only JPG, JPEG, PNG, GIF files are allowed.";
+				echo "Sorry, only JPG, JPEG, PNG, GIF files are allowed.<br>";
 				$uploadOk = 0;
 			}
 		}
 
 		if ($uploadOk != 0) {
 			if (!move_uploaded_file($file["tmp_name"], $target_file)) {
-				echo "Some problems with " . $file["name"];
+				echo "Some problems with " . $file["name"] . "<br>";
 			} 
 		}
 	}
@@ -90,3 +108,12 @@ function printTable($dir) {
 	$output .= "</table>";
 	echo $output;
 }
+?>
+
+<a href="" id="backLink">Back</a>
+<script type="text/javascript">
+	document.getElementById("backLink").href = document.referrer;
+</script>
+
+</body>
+</html>
